8968965c986b1e4bc25c23a4084c0a8986a82f61
Ben McManus <user@server.loc>
2014-02-02 12:25:13 +0000

    added timestamp to element src

---
+++ app/code/community/Hackathon/StaticAssets/Block/Html/Head.php
@@ -0,0 +1,68 @@
+<?php
+
+class Hackathon_StaticAssets_Block_Html_Head
+    extends Mage_Page_Block_Html_Head
+{
+    /**
+     * Merge static and skin files of the same format into 1 set of HEAD directives or even into 1 directive
+     *
+     * Will attempt to merge into 1 directive, if merging callback is provided. In this case it will generate
+     * filenames, rather than render urls.
+     * The merger callback is responsible for checking whether files exist, merging them and giving result URL
+     *
+     * @param string $format - HTML element format for sprintf('<element src="%s"%s />', $src, $params)
+     * @param array $staticItems - array of relative names of static items to be grabbed from js/ folder
+     * @param array $skinItems - array of relative names of skin items to be found in skins according to design config
+     * @param callback $mergeCallback
+     * @return string
+     */
+    protected function &_prepareStaticAndSkinElements($format, array $staticItems, array $skinItems, $mergeCallback = null)
+    {
+        $designPackage = Mage::getDesign();
+        $baseJsUrl = Mage::getBaseUrl('js');
+        $items = array();
+        if ($mergeCallback && !is_callable($mergeCallback)) {
+            $mergeCallback = null;
+        }
+
+        // get static files from the js folder, no need in lookups
+        foreach ($staticItems as $params => $rows) {
+            foreach ($rows as $name) {
+                $items[$params][] = $mergeCallback ? Mage::getBaseDir() . DS . 'js' . DS . $name : $baseJsUrl . $name;
+            }
+        }
+
+        // lookup each file basing on current theme configuration
+        foreach ($skinItems as $params => $rows) {
+            foreach ($rows as $name) {
+                $items[$params][] = $mergeCallback ? $designPackage->getFilename($name, array('_type' => 'skin'))
+                    : $designPackage->getSkinUrl($name, array());
+            }
+        }
+
+        $html = '';
+        foreach ($items as $params => $rows) {
+            // attempt to merge
+            $mergedUrl = false;
+            if ($mergeCallback) {
+                $mergedUrl = call_user_func($mergeCallback, $rows);
+            }
+            // render elements
+            $params = trim($params);
+            $params = $params ? ' ' . $params : '';
+            if ($mergedUrl) {
+                $html .= sprintf($format, $mergedUrl, $params);
+            } else {
+                foreach ($rows as $src) {
+                    $html .= sprintf($format, $this->_assetCache($src), $params);
+                }
+            }
+        }
+        return $html;
+    }
+
+    protected function _assetCache($src)
+    {
+        return $src . '?v=' . gmmktime();
+    }
+} 
